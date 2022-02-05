5a4fabbabe0e18153b9547aa020f5133bb226f8e
Jogvan <jogvan@gmail.com>
2022-02-05 03:17:04 +0000

    Add the calculator buttons and calculation functions

---
+++ script.js
@@ -0,0 +1,25 @@
+let result = 0;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, c) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(event){
+    let operator = event.target.value
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+  }
