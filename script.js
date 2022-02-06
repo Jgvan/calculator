d22dae28090ecc8f750717581d119f145c1b956a
Jogvan <jogvan@gmail.com>
2022-02-06 02:46:03 +0000

    Add the rest of the calculator buttons and style. Wire up the number buttons to the display

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let result = 0;
+let input = "0";
+
+//Elements
+const numpad = document.querySelectorAll(".numpad");
+const displayInput = document.querySelector(".input");
+
+//Button bindings
+
+numpad.forEach(btn => btn.addEventListener("click", numberInput));
 
 function add(a, b) {
     return a + b;
@@ -16,10 +25,34 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(event){
+function operate(event) {
     let operator = event.target.value
 }
 
 function add(num1, num2) {
     return num1 + num2;
-  }
+}
+
+function clearAll(){
+    result = 0;
+    input = 0;
+    updateDisplayInput();
+}
+
+function numberInput(event) {
+    let number = event.target.value;
+    if (input == "0") {
+        input = number;
+    }
+    else {
+        input += number;
+    }
+    updateDisplayInput();
+}
+
+function updateDisplayInput() {
+    displayInput.textContent = input;
+}
+
+
+clearAll();
